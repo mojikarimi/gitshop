d9358a7e1f30928f3e87c0839cb313d1cef51b51
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-02-25 03:28:59 +0330

    edit and create profile templates and edit & create view app profile

--- Profile/urls.py
+++ Profile/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 
 urlpatterns = [
     re_path(r'^profile_dashboard$', profile_dashboard, name='profile_dashboard'),
+    re_path(r'^profile_personal_info$', profile_personal_info, name='profile_personal_info'),
     re_path(r'^profile_tickets$', profile_tickets, name='profile_tickets'),
     re_path(r'^profile_detail_tickets/(?P<pk>\d+)$', profile_detail_tickets, name='profile_detail_tickets'),
 
--- Profile/views.py
+++ Profile/views.py
@@ -8,7 +8,7 @@ from jdatetime import datetime
 from django.conf import settings
 from Profile.models import Tickets, AnswerTicket
 from Account.models import CustomUser
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 
 
 @login_required
@@ -19,7 +19,7 @@ def profile_dashboard(request):
         user = CustomUser.objects.get(pk=request.user.pk)
         user.verify_code = x
         user.save()
-        send_mail(subject='moji', message=f'hi moji {x}', from_email=settings.EMAIL_HOST_USER,
+        send_mail(subject='moji', message='hi moji ' + x, from_email=settings.EMAIL_HOST_USER,
                   recipient_list=[request.user.email])  # Send email
         return redirect('verify_email')
     myuser = CustomUser.objects.get(pk=request.user.pk)
@@ -29,7 +29,37 @@ def profile_dashboard(request):
 @login_required
 def profile_personal_info(request):
     myuser = CustomUser.objects.get(pk=request.user.pk)
-    return render(request, 'front/Profile/profile_dashboard.html', {'myuser': myuser, })
+    if request.method == 'POST':
+        last_name = request.POST.get('lname')
+        first_name = request.POST.get('fname')
+        phone_number = request.POST.get('phone_number')
+        national_number = request.POST.get('national_number')
+        card_number = request.POST.get('card_number')
+        image = request.FILES.get('image')
+        email = request.POST.get('email')
+        myuser.last_name = last_name
+        myuser.first_name = first_name
+        myuser.phone_number = phone_number
+        myuser.national_number = national_number
+        myuser.card_number = card_number
+        if image:
+            myuser.image = image
+        if email != myuser.email:
+            x = ''.join((random.choice('0123456789') for _ in range(5)))
+            # Creating a verification code
+            request.session['username'] = request.user.username
+            myuser.verify_code = x
+            myuser.is_active = False
+            myuser.email = email
+            myuser.save()
+            send_mail(subject='moji', message='hi moji ' + x, from_email=settings.EMAIL_HOST_USER,
+                      recipient_list=[request.user.email])  # Send email
+            return redirect('verify_email')
+        else:
+            myuser.save()
+            return redirect('profile_personal_info')
+
+    return render(request, 'front/Profile/profile_personal_info.html', {'myuser': myuser, })
 
 
 @login_required
@@ -37,6 +67,7 @@ def profile_tickets(request):
     if not request.user.is_active:  # Account activation notification
         messages.warning(request, 'Please activate your account')
         return redirect('profile_dashboard')
+    myuser = CustomUser.objects.get(pk=request.user.pk)
     parts = Group.objects.using('profile').all()
     tickets = Tickets.objects.using('profile').order_by('-date').filter(user_id=request.user.pk)
     for i in tickets.filter(status='درحال بررسی'):
@@ -45,7 +76,7 @@ def profile_tickets(request):
                 (int(i.date[:4]) * 365) + (int(i.date[5:7]) * 30) + (int(i.date[8:10]))) >= 10:
             i.status = 'بسته شده'
             i.save(using='profile')
-    context = {'parts': parts, 'tickets': tickets}
+    context = {'parts': parts, 'tickets': tickets, 'myuser': myuser}
     if request.method == 'POST':
         subject = request.POST.get('subject')
         part = request.POST.get('part')
@@ -65,9 +96,10 @@ def profile_detail_tickets(request, pk):
     if not request.user.is_active:  # Account activation notification
         messages.warning(request, 'Please activate your account')
         return redirect('profile_dashboard')
+    myuser = CustomUser.objects.get(pk=request.user.pk)
     ticket = Tickets.objects.using('profile').get(pk=pk, user_id=request.user.pk)
     answertickets = AnswerTicket.objects.using('profile').order_by('-date').filter(ticket=ticket)
-    context = {'ticket': ticket, 'answertickets': answertickets, 'number': len(answertickets)}
+    context = {'ticket': ticket, 'answertickets': answertickets, 'number': len(answertickets), 'myuser': myuser}
     if request.method == 'POST':
         text = request.POST.get('text')
         type_user = request.POST.get('type_user')
